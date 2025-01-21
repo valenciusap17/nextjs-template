2c4115dd0447f3ff91f880bffc9e3aea071213ca
valenciusap17 <v.apriady@gmail.com>
2025-01-22 00:15:30 +0700

    feat: setup eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+import js from "@eslint/js";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import importPlugin from "eslint-plugin-import-x";
+
+const customStylistic = stylistic.configs.customize({
+  semi: false,
+  quites: "single",
+});
+
+export default tseslint.config(
+  { ignores: ["node_modules"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  {
+    ...customStylistic,
+    rules: {
+      ...customStylistic.rules,
+      "@stylistic/operator-linebreak": "off",
+    },
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  {
+    ...importPlugin.flatConfigs.recommended,
+    rules: {
+      "import-x/extension": ["error", "always", { ignorePackages: true }],
+    },
+  },
+);
